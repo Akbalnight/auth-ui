768a8dbdaded7b7f283d94e0f382cef174349121
Erokhin Mikhail <merokhin@appliedtech.ru>
2019-02-27 15:05:02 +0500

    Добавлены экспорты actions, utils

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Login from './components/Login';
 import CheckAccess from './components/CheckAccess';
 import auth from './reducers';
+import authRequestResult from './actions';
+import { checkAuthRequest, doLogoutRequest } from './components/Login/authUtils';
 
-export { Login, CheckAccess, auth };
+export { Login, CheckAccess, auth, authRequestResult, checkAuthRequest, doLogoutRequest };
