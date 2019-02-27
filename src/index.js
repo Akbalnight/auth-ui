2d418d834a3ff0aaf411664a257ce7974d4b3e4a
Erokhin Mikhail <merokhin@appliedtech.ru>
2019-02-27 15:24:52 +0500

    Фикс экспорта actions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Login from './components/Login';
 import CheckAccess from './components/CheckAccess';
 import auth from './reducers';
-import authRequestResult from './actions';
+import { authRequestResult } from './actions';
 import { checkAuthRequest, doLogoutRequest } from './components/Login/authUtils';
 
 export { Login, CheckAccess, auth, authRequestResult, checkAuthRequest, doLogoutRequest };
