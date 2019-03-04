5fce462d2d349274615a36c9018cbc3764689f40
Elena Shishkina <eshishkina@appliedtech.ru>
2019-03-04 22:47:16 +0500

    Set loading on AUTH_REQUEST_START

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -12,7 +12,7 @@ const initialState = {
 const auth = (state = initialState, action) => {
     switch (action.type) {
         case types.AUTH_REQUEST_START:
-            return initialState;
+            return Object.assign({}, initialState, {loading: true});
         case types.AUTH_REQUEST_RESULT:
             if (action.result) {
                 let permissions = {};
