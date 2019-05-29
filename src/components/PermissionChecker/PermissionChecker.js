e1d022398ba93165ec16a590ff81be3c7a06136f
Dmitry Danilin <ddaniln@appliedtech.ru>
2019-05-29 02:32:44 -0700

    HLASKUTE-1120: Добавлена генерация кэша для упрощённой проверки привелегй

--- src/components/PermissionChecker/PermissionChecker.js
+++ src/components/PermissionChecker/PermissionChecker.js
@@ -5,14 +5,60 @@ const checker = (state = {}, permission) => {
     return permissions[permission]
 };
 
+const combinePath = (a, b) => a ? `${a}.${b}` : b;
+
+const recursiveCacheGenerator = (state, tree, path = null, cache) => {
+  if (Array.isArray(tree)) {
+    cache[path] = tree.map(p =>
+      (state.permissions && state.permissions[p]) ||
+      (Array.isArray(state.roles) && state.roles.includes(p))
+    ).some(p => p);
+
+    return cache[path];
+  } else if (typeof tree === 'object') {
+    const keys = Object.keys(tree);
+    const childrenCan = keys.map(key =>
+      recursiveCacheGenerator(state, tree[key], combinePath(path, key), cache)
+    ).some(p => p);
+
+    if (path)
+      cache[path] = childrenCan;
+
+    return childrenCan;
+  }
+
+  return false;
+};
+
+const updatePermissionsCache = (state, tree) => {
+  if (state) {
+    const cache = {};
+    recursiveCacheGenerator(state, tree, '', cache);
+    return cache;
+  }
+  return {};
+};
+
 export class PermissionChecker{
-    constructor(reduxStore){
-        this.state = getAuth(reduxStore.getState());
-        reduxStore.subscribe(() => {
+    constructor(reduxStore, permissionsTreeMapping = {}){
+        const updateState = () => {
             this.state = getAuth(reduxStore.getState());
-        })
+            this.cache = updatePermissionsCache(this.state, permissionsTreeMapping);
+        };
+
+        updateState();
+        reduxStore.subscribe(updateState);
     }
+
+    hasPermission(permission) {
+      return !!checker(this.state, permission);
+    }
+
+    hasInternalRole(role) {
+      return !!this.cache[role]
+    }
+
     has(permission) {
-        return checker(this.state, permission);
+        return this.hasInternalRole(permission) || this.hasPermission(permission);
     }
 }
