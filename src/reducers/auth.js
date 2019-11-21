c7927fefceb78a0283f1d619657a372d281685a1
elena.shishkina <shishkina.r74@gmail.com>
2019-11-21 17:24:09 +0300

    ASSD-275 Разные форматы ответов для login

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -7,7 +7,31 @@ const initialState = {
     roles: [],
     permissions: {}
 };
-
+const getResultForToken = res => {
+  const permissions ={};
+  const roles =res.roles.split(', ');
+  roles[0] = roles[0].slice(1);
+  roles[roles.length-1] =  roles[0].slice(0, roles.length-1);
+  return {
+    loading: false,
+    access_token: res.access_token,
+    refresh_token: res.refresh_token,
+    loggedIn: true,
+    id: res.userId,
+    username: res.username,
+    email: res.email || '',
+    avatarUrl:  null,
+    firstName:  null,
+    middleName:  null,
+    lastName: null,
+    position:  null,
+    phone:  null,
+    permissions:  res.permissions.forEach(p => {
+      permissions[p.path.replace(/\/+$/, "") + ':' + p.method] = true;
+    }),
+    roles
+  };
+};
 
 const auth = (state = initialState, action) => {
     switch (action.type) {
@@ -15,24 +39,18 @@ const auth = (state = initialState, action) => {
             return Object.assign({}, initialState, {loading: true});
         case types.AUTH_REQUEST_RESULT:
             if (action.result) {
-                let permissions = {};
-                action.result.roles.forEach(r => r.permissions.forEach(p => {
-                    permissions[p.path.replace(/\/+$/, "") + ':' + p.method] = true;
-                }));
-                const userData = action.result.jsonData;
-                let roles;
-                if(Array.isArray(action.result.roles)){
-                  roles = action.result.roles.map(r => r.name);
+                if(action.result.access_token) {
+                  return getResultForToken(action.result);
                 } else {
-                  const arr = action.result.roles.split(', ');
-                  arr[0] = arr[0].slice(1);
-                  arr[arr.length-1] =  arr[0].slice(0, arr.length-1);
-                  roles = arr;
-                }
-                return {
+                  let permissions = {};
+                  action.result.roles.forEach(r => r.permissions.forEach(p => {
+                    permissions[p.path.replace(/\/+$/, "") + ':' + p.method] = true;
+                  }));
+                  const userData = action.result.jsonData;
+                  return {
                     loading: false,
                     loggedIn: true,
-                  	id: action.result.id,
+                    id: action.result.id,
                     username: action.result.name,
                     email: action.result.email,
                     avatarUrl: userData && 'avatarUrl' in userData ? userData.avatarUrl : null,
@@ -42,8 +60,9 @@ const auth = (state = initialState, action) => {
                     position: userData && 'position' in userData ? userData.position : null,
                     phone: userData && 'phone' in userData ? userData.phone : null,
                     permissions: permissions,
-                    roles
-                };
+                    roles: action.result.roles.map(r => r.name)
+                  };
+                }
             } else {
                 return {
                     loading: false,
